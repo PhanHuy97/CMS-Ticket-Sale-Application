4bd190a9cf74b5fba7e58283bccf3f2b99b389ce
Huy Phan <phanquochuy719@gmail.com>
2022-01-21 15:31:44 +0700

    tien do ngay 20/1/2022

--- src/component/calendar/index.tsx
+++ src/component/calendar/index.tsx
@@ -1,75 +1,43 @@
+import { Calendar } from "antd";
+import moment from "moment";
+import "moment/locale/vi";
+import locale from "antd/lib/date-picker/locale/vi_VN";
 import style from "./calendar.module.scss";
+import HeaderRender from "./header";
+import "./calendar.scss";
+import clsx from "clsx";
 
 function CalendarCustom() {
-  // Xác đinh là thứ mấy
-  function Zeller(D: number, M: number, Y: number) {
-    if (M < 3) {
-      M = M + 12;
-      Y = Y - 1;
-    }
+  moment.locale("vi");
 
-    var C = Math.floor(Y / 100);
-    var K = Y - 100 * C;
+  let selectedValue: moment.Moment;
 
-    var S =
-      Math.floor(2.6 * M - 5.39) +
-      Math.floor(K / 4) +
-      Math.floor(C / 4) +
-      D +
-      K -
-      2 * C;
+  const DateFullCellRender = (value: moment.Moment) => {
+    console.log(value);
+    return (
+      <div>
+        <span className={style.date}>{value.date()}</span>
+      </div>
+    );
+  };
 
-    var ans = S - 7 * Math.floor(S / 7);
-    return ans;
-  }
+  const handleSelected = (value: moment.Moment) => {
+    selectedValue = value;
+    console.log(selectedValue);
+  };
 
-  // xác định năm nhuận
-  function isLeap(year: number) {
-    if (year % 4 || (year % 100 === 0 && year % 400)) return 0;
-    else return 1;
-  }
-
-  // tìm số ngày mỗi tháng
-  function daysIn(month: number, year: number) {
-    return month === 2 ? 28 + isLeap(year) : 31 - (((month - 1) % 7) % 2);
-  }
-
-  // lập lịch tháng
-  function calendar(month: number, year: number) {
-    var startIndex = Zeller(1, month, year);
-    var endIndex = daysIn(month, year);
-    var result = Array.apply(0, Array(35)).map(function (i) {
-      return 0;
-    });
-    if (startIndex === 0) {
-      for (var i = startIndex; i < endIndex + startIndex; i++) {
-        result[i + 6] = i - startIndex + 1;
-      }
-    } else {
-      for (var i: number = startIndex; i < endIndex + startIndex; i++) {
-        result[i - 1] = i - startIndex + 1;
-      }
-    }
-    return result;
-  }
-  const monthnames = new Array(
-    "January",
-    "Februrary",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "Decemeber"
+  return (
+    <div className={style.wapper}>
+      <Calendar
+        className={clsx(style.calendar, "calendar-custom")}
+        fullscreen={false}
+        locale={locale}
+        headerRender={HeaderRender}
+        // dateFullCellRender={DateFullCellRender}
+        onSelect={handleSelected}
+      />
+    </div>
   );
-  let todayDate = new Date();
-  let thismonth = todayDate.getMonth() + 1;
-  let thisyear = todayDate.getFullYear();
-  return <div></div>;
 }
 
 export default CalendarCustom;
